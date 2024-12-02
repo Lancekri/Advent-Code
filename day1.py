a5b372e99570bf79083980714f53df9646844a35
Lancekri <137403430+Lancekri@users.noreply.github.com>
2024-12-02 12:21:21 +0100

    Update day1.py

--- day1.py
+++ day1.py
@@ -1,9 +1,12 @@
 def main():
   file = open("day1_input.txt", "r")
+  list1 = []
+  list2 = []
   for line in file: 
-    print(line)
-list1 = sorted([])
-list2 = sorted([])
+    list1.append(line[0])
+    list2.append(line[1])
+list1.sort()
+list2.sort()
 distance = 0
 for i in range(list1.length()):
   distance += abs(list1[i] - list2[i])
